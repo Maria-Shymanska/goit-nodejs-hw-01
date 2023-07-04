a08e60ce0814fdd11d611a387780d200996cbb7f
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-07-04 17:49:25 +0200

    add gitignore

---
+++ index.js
@@ -0,0 +1,41 @@
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-n, --name, <type>")
+  .option("-e, --email, <type>")
+  .option("-p, --phone, <type>");
+
+program.parse();
+const options = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(options);
